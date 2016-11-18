0bde036c8c3a89001074240b520455b0c4c40682
yachang <yachang@users.noreply.github.com>
2016-11-18 14:34:26 -0500

    Update upload_bigstore.go

--- upload_bigstore.go
+++ upload_bigstore.go
@@ -1,4 +1,17 @@
-// Check the content of source bucket, and move them to destination bucket.
+/*
+Copyright 2013 Google Inc.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+	http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// Check the content of source bucket, and sync them to destination bucket.
 
 package main
 
@@ -77,6 +90,7 @@ func main() {
     existing_filenames[OneItem.Name] = true
   }
 
+  var added_filenames []string
   for _, OneItem := range source_files.Items {
     // Check whether the files is already in the destination.
     fmt.Printf("Handling source file: %s\n", OneItem.Name)
@@ -88,11 +102,18 @@ func main() {
       // Insert the object into destination bucket.
       object := &storage.Object{Name: OneItem.Name}
       if res, err := service.Objects.Insert(*DestBucketName, object).Media(file_content.Body).Do(); err == nil {
-        fmt.Printf("Created object %v at location %v\n", res.Name, res.SelfLink)
+        //fmt.Printf("Created object %v at location %v\n", res.Name, res.SelfLink)
+        added_filenames = append(added_filenames, OneItem.Name)
       } else {
-        fmt.Printf("Objects.Insert failed: %v\n", err)
+        fmt.Printf("Objects insert failed: %v\n", err)
         os.Exit(1)
       }
     }
-  }      
+  }
+
+  // Generate Log for newly added tar files.
+  fmt.Printf("Added files in bucket:\n")
+  for _, object := range added_filenames {
+    fmt.Println(object)
+  }    
 }
