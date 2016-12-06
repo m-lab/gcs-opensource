0af78e8dafe4947c730f13d12541d9c093b05739
yachang <yachang@users.noreply.github.com>
2016-12-06 14:45:39 -0500

    Update gcs_operations_test.go

--- gcs_operations_test.go
+++ gcs_operations_test.go
@@ -1,41 +1,47 @@
-package gcs_operation
+/*
+Copyright 2013 Google Inc.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+	http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package gcs
 
 import (
-  "fmt"
-  //"os"
-  //"path/filepath"
-  "testing"
-
-  //"golang.org/x/net/context"
-  //"golang.org/x/oauth2/google"
-  //storage "google.golang.org/api/storage/v1"
+	"fmt"
+	"testing"
 )
 
 func TestBucketCreation(t *testing.T) {
-  bucket_name := "test-bucket-gcs-operations"
-  result := CreateBucket("mlab-oti", bucket_name)
-  if result == false {
-    t.Errorf("Cannot create bucket")
-    return
-  }
+	bucket_name := "test-bucket-gcs-operations"
+	result := CreateBucket("mlab-oti", bucket_name)
+	if result == false {
+		t.Errorf("Cannot create bucket")
+		return
+	}
 
-  result = CopyOneFile("tarfile_raw_data", bucket_name, "search.png")
-  if result == false {
-    t.Errorf("Cannot copy file from another bucket.")
-    return
-  }
+	result = CopyOneFile("tarfile_raw_data", bucket_name, "search.png")
+	if result == false {
+		t.Errorf("Cannot copy file from another bucket.")
+		return
+	}
 
-  file_names := GetFileNamesFromBucket(bucket_name)
+	file_names := GetFileNamesFromBucket(bucket_name)
 
-  fmt.Printf("Files in bucket %v:\n", bucket_name)
-  for _, file_name := range file_names {
-    fmt.Println(file_name)
-  }
+	fmt.Printf("Files in bucket %v:\n", bucket_name)
+	for _, file_name := range file_names {
+		fmt.Println(file_name)
+	}
 
-  result = DeleteFiles(bucket_name, "")
-  if result == false {
-    t.Errorf("Cannot delete bucket")
-    return
-  } 
+	result = DeleteFiles(bucket_name, "")
+	if result == false {
+		t.Errorf("Cannot delete bucket")
+		return
+	}
 }
-
